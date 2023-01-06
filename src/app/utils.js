b35b48f238240271044b412949a77db5f92f93bd
alihassan1177 <ah927481@gmail.com>
2023-01-07 03:07:14 +0500

    just got an idea to create dynamic strings

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,6 +1,4 @@
-export const PageTransition = {
-      initial:{opacity : 0},
-      animate:{opacity : 1},
-      transition:{delay : 0.1},
-      exit:{opacity : 0},
+export const strings = {
+  messages : {},
+  errors : {}
 }
